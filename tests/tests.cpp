d31283c4dc4190597b0b4365f37e261ffdf4ff68
diyooor <diyorsattarov7@gmail.com>
2023-12-22 00:20:16 -0600

    Spawned vector of slaves

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,27 +4,35 @@
 #include "slave.cpp"
 
 int main() {
-    // Initialize master and slave with appropriate parameters
-    unsigned short port = 8080;
-    std::string taskMessage = "Task for slave";
+    int port = 8080;
+    std::string taskMessage = "task for Slave";
+    // Initialize the master
     Master master(port, taskMessage);
-    Slave slave("localhost", port); // Assuming the Slave connects to localhost
+    std::thread masterThread([&master]() { master.startServer(); });
 
-    // Create and start the master and slave in separate threads
-    std::thread masterThread([&master](){
-        master.startServer();
-    });
+    // Vector to hold slave threads
+    std::vector<std::thread> slaveThreads;
+    const int numberOfSlaves = 5; // Adjust the number of slaves as needed
 
-    // Give the master server some time to start up
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // Create and run slave threads
+    for (int i = 0; i < numberOfSlaves; ++i) {
+        slaveThreads.emplace_back([&, i]() {
+            Slave slave("localhost", port); // Create a new slave instance
+            slave.connectToMaster();       // Connect each slave to the master
+        });
+    }
 
-    std::thread slaveThread([&slave](){
-        slave.connectToMaster(); // Assuming this method initiates the slave's operations
-    });
+    // Join all slave threads
+    for (auto& thread : slaveThreads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
 
-    // Join threads to wait for their completion
-    masterThread.join();
-    slaveThread.join();
+    // Join the master thread
+    if (masterThread.joinable()) {
+        masterThread.join();
+    }
 
     return 0;
 }
